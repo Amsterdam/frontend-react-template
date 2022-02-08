341b436db346af30f6cc8a0eeabc3e01a3e95611
Jasper Swart <jasper.g.swart@gmail.com>
2022-02-08 15:34:44 +0100

    added ASC to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GlobalStyle, ThemeProvider, Row, Column, Heading } from "@amsterdam/asc-ui";
 
+console.log(1);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <ThemeProvider>
+        <GlobalStyle />
+        <Row>
+          <Column span={12}>
+            <Heading>Home</Heading>
+          </Column>
+        </Row>
+      </ThemeProvider>
+    </React.StrictMode>
   );
 }
 
