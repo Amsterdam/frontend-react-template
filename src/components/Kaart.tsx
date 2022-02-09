b5a8eab23952fa62dca5214737e842fadb7901ff
Jasper Swart <jasper.g.swart@gmail.com>
2022-02-09 13:51:05 +0100

    added map

--- src/components/Kaart.tsx
+++ src/components/Kaart.tsx
@@ -1,11 +1,31 @@
-// import { Link } from "react-router-dom";/
+import "leaflet/dist/leaflet.css";
+import styled from "styled-components";
+import { Map, ViewerContainer, BaseLayer, Zoom } from "@amsterdam/arm-core";
 import { Heading } from "@amsterdam/asc-ui";
+
+const StyledMap = styled(Map)`
+  width: 100%;
+  height: 400px;
+`;
+
+const StyledDiv = styled.div`
+  width: 100%;
+`;
 
 const Kaart = () => {
   return (
-    <>
-      <Heading>Kaart</Heading>
-    </>
+    <StyledDiv>
+      <div>
+        <Heading>Kaarten</Heading>
+      </div>
+
+      <div>
+        <StyledMap>
+          <ViewerContainer bottomRight={<Zoom />} />
+          <BaseLayer />
+        </StyledMap>
+      </div>
+    </StyledDiv>
   );
 };
 
