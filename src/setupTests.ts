85e01ec6c5a526dc4a4c8a8f01dadeea8408f071
Jasper Swart <jasper.g.swart@gmail.com>
2022-02-10 00:04:08 +0100

    formatted

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,7 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-
 import "jest-styled-components";
 
 Object.defineProperty(window, "matchMedia", {
