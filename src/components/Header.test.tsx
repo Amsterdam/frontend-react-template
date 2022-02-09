7d025cdfb7f84fe74fd33499ee6d3b1ee7a38f6f
Jasper Swart <jasper.g.swart@gmail.com>
2022-02-09 14:59:05 +0100

    added header test

---
+++ src/components/Header.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { withTheme } from "../test/utils";
+import Header from "./Header";
+
+describe("Header", () => {
+  it("renders correctly", () => {
+    render(withTheme(<Header/>));
+
+    expect(screen.getByTestId("header")).toBeInTheDocument();
+
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("Kaart")).toBeInTheDocument();
+  });
+});
+
