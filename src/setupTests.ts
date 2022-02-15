788f8e533f14339e5f98e77b63e4241e4ef07383
Jasper Swart <jasper.g.swart@gmail.com>
2022-02-15 16:54:58 +0100

    removed matchMedia from test setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,17 +4,3 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 import "jest-styled-components";
-
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // Deprecated
-    removeListener: jest.fn(), // Deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
