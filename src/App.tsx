bfe70ab31a62b25cae3bba2835ec2c6fdb7c92e3
Jasper Swart <jasper.g.swart@gmail.com>
2022-02-15 17:38:08 +0100

    moved GlobalStyle, ThemeProvider to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { GlobalStyle, ThemeProvider, Row, Column } from "@amsterdam/asc-ui";
+import { Row, Column } from "@amsterdam/asc-ui";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Map from "./components/Map";
 
+console.log("App");
+
 function App() {
   return (
-    <React.StrictMode>
-      <ThemeProvider>
-        <GlobalStyle />
+    <Router>
+      <Row>
+        <Column span={12}>
+          <Header />
+        </Column>
+      </Row>
 
-        <Router>
-          <Row>
-            <Column span={12}>
-              <Header />
-            </Column>
-          </Row>
+      <Row>
+        <Column span={12}>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/kaart" element={<Map />} />
+          </Routes>
+        </Column>
+      </Row>
 
-          <Row>
-            <Column span={12}>
-              <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/kaart" element={<Map />} />
-              </Routes>
-            </Column>
-          </Row>
-
-          <Footer />
-        </Router>
-      </ThemeProvider>
-    </React.StrictMode>
+      <Footer />
+    </Router>
   );
 }
 
