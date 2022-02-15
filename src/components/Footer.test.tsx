627d05fac8500426e85cb56c441b661e0aee0225
Jasper Swart <jasper.g.swart@gmail.com>
2022-02-15 16:56:01 +0100

    added Footer test

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -2,9 +2,24 @@ import { render } from "@testing-library/react";
 import { withTheme } from "../test/utils";
 import Footer from "./Footer";
 
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+
 describe("Footer", () => {
   it("renders correctly", () => {
-    // render(withTheme(<Footer />));
+    render(withTheme(<Footer />));
 
     // expect(screen.getByTestId("footer")).toBeInTheDocument();
 
