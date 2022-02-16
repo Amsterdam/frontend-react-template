e584a2ed33e60bf5daa943bba2a0a16c469b0c10
janthijs <92784122+janthijs@users.noreply.github.com>
2022-02-16 11:37:45 +0100

    fix: resolve issue

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -1,21 +1,7 @@
+import "../matchMedia.mock";
 import { render } from "@testing-library/react";
 import { withTheme } from "../test/utils";
 import Footer from "./Footer";
-
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // Deprecated
-    removeListener: jest.fn(), // Deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
-
 
 describe("Footer", () => {
   it("renders correctly", () => {
