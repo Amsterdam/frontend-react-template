c86a6ef828ef55242e0ab2a539d6dd8b8fed0869
Jasper Swart <jasper.g.swart@gmail.com>
2022-02-16 11:10:28 +0100

    added active to unit test

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -2,6 +2,13 @@ import { render, screen } from "@testing-library/react";
 import { withTheme } from "../test/utils";
 import Header from "./Header";
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useLocation: () => ({
+    pathname: "/home"
+  })
+}));
+
 describe("Header", () => {
   it("renders correctly", () => {
     render(withTheme(<Header />));
@@ -10,5 +17,8 @@ describe("Header", () => {
 
     expect(screen.getByText("Home")).toBeInTheDocument();
     expect(screen.getByText("Kaart")).toBeInTheDocument();
+
+    // home should be active now
+    expect(screen.getByTestId("header-home-button")).toHaveStyleRule('color', "#ec0000");
   });
 });
