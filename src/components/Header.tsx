9640089af91bd179a21c1770f9e74adedcb87d88
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-04 14:05:08 +0100

    formatted

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Header as ASCHeader, MenuItem, MenuButton, MenuInline } from "@amsterda
 
 const Header = () => {
   const location = useLocation();
-  
+
   return (
     <ASCHeader
       tall={false}
@@ -15,8 +15,13 @@ const Header = () => {
         <>
           <MenuInline>
             <MenuItem>
-              {/* @ts-ignore */}
-              <MenuButton as={Link} to="/home" active={!!matchPath(location.pathname, "/home")} data-testid="header-home-button">
+              <MenuButton
+                as={Link}
+                {/* @ts-ignore */}
+                to="/home"
+                active={!!matchPath(location.pathname, "/home")}
+                data-testid="header-home-button"
+              >
                 Home
               </MenuButton>
             </MenuItem>
