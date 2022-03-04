b39c03d731b1a01f2bd3935408f83473e7d1d447
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-04 13:55:29 +0100

    added spacing to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,20 @@
 import styled from "styled-components";
-import { Heading } from "@amsterdam/asc-ui";
+import { Heading, themeSpacing } from "@amsterdam/asc-ui";
 
 const StyledDiv = styled.div`
+  margin-top: ${themeSpacing(10)};
   width: 100%;
+`;
+
+const StyledHeading = styled(Heading)`
+  margin-bottom: ${themeSpacing(5)};
 `;
 
 const Home = () => {
   return (
     <>
       <StyledDiv data-testid="home"> 
-        <Heading>Home</Heading>
+        <StyledHeading>Home</StyledHeading>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
