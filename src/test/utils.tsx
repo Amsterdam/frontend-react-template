839b7da8639d4377073b7287966fdc34a83bd8cb
Jasper Swart <jasper.g.swart@gmail.com>
2022-05-29 01:53:25 +0200

    refactored to ts

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,10 +1,11 @@
 import { createMemoryHistory } from "history";
 import { ThemeProvider } from "@amsterdam/asc-ui";
 import { unstable_HistoryRouter as Router } from "react-router-dom"
+import { ReactNode } from "react";
 
 export const history = createMemoryHistory();
 
-export const withTheme = (Component) => {
+export const withTheme = (Component: ReactNode) => {
   return (
     <ThemeProvider>
       <Router history={history}>{Component}</Router>
