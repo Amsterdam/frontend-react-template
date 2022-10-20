cb56e2d56d31be12f2397831f43872d6a7f2ae4b
Jasper Swart <jasper.g.swart@gmail.com>
2022-10-20 22:15:23 +0200

    added useFetchData

--- src/hooks/useFetchData.test.ts
+++ src/hooks/useFetchData.test.ts
@@ -1,95 +1,35 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 import axios from "axios";
+
 import useFetchData from "./useFetchData";
 
 jest.mock("axios");
 
-describe("hooks/useFetchData", () => {
+describe("useFetchData", () => {
   const mockData = {
     data: "mock-data",
   };
+
+  beforeEach(() => {
+    //@ts-ignore
+    axios.get.mockImplementation(() => Promise.resolve(mockData));
+  });
 
   afterEach(() => {
     jest.resetAllMocks();
   });
 
-  it("should do a GET request", async () => {
-    //@ts-ignore
-    axios.get.mockResolvedValueOnce({ data: mockData, statusText: "OK" });
-
-    const { result, unmount } = renderHook(() => useFetchData());
-
-    await act(async () => result.current.get("http://test-url"));
-
-    expect(result.current.data).toEqual(mockData);
-    expect(result.current.error).toBeFalsy();
-    expect(result.current.loading).toBeFalsy();
-
-    unmount();
+  it("should create the hook", () => {
+    const { result } = renderHook(() => useFetchData());
+    expect(result.current).not.toBeUndefined();
+    expect(result.current.loading).toEqual(false);
+    expect(result.current.errorMessage).toBeUndefined();
   });
 
-  it("should return errors when doing a GET", async () => {
-    //@ts-ignore
-    axios.get.mockResolvedValueOnce({ data: mockData, statusText: "false" });
-
-    const { result, unmount } = renderHook(() => useFetchData());
-
-    await act(async () => result.current.get("http://test-url"));
-
-    expect(result.current.error).toBeTruthy();
-    expect(result.current.loading).toBeFalsy();
-
-    unmount();
-  });
-
-  it("should do a POST request", async () => {
-    //@ts-ignore
-    axios.post.mockResolvedValueOnce({ data: mockData, statusText: "OK" });
-
-    const { result, unmount } = renderHook(() => useFetchData());
-
-    const patchObject = {};
-
-    await act(async () => result.current.post("http://test-url", patchObject));
-
-    expect(result.current.data).toEqual(mockData);
-    expect(result.current.error).toBeFalsy();
-    expect(result.current.loading).toBeFalsy();
-
-    unmount();
-  });
-
-  it("should do a PATCH request", async () => {
-    //@ts-ignore
-    axios.patch.mockResolvedValueOnce({ data: mockData, statusText: "OK" });
-
-    const { result, unmount } = renderHook(() => useFetchData());
-
-    const patchObject = {};
-
-    await act(async () => result.current.patch("http://test-url", patchObject));
-
-    expect(result.current.data).toEqual(mockData);
-    expect(result.current.error).toBeFalsy();
-    expect(result.current.loading).toBeFalsy();
-
-    unmount();
-  });
-
-  it("should do a PUT request", async () => {
-    //@ts-ignore
-    axios.put.mockResolvedValueOnce({ data: mockData, statusText: "OK" });
-
-    const { result, unmount } = renderHook(() => useFetchData());
-
-    const patchObject = {};
-
-    await act(async () => result.current.put("http://test-url", patchObject));
-
-    expect(result.current.data).toEqual(mockData);
-    expect(result.current.error).toBeFalsy();
-    expect(result.current.loading).toBeFalsy();
-
-    unmount();
+  it("should fetch the data", async () => {
+    const { result } = renderHook(() => useFetchData());
+    await act(async () => result.current.fetchData("http://test-url"));
+    expect(result.current.errorMessage).toBeUndefined();
+    expect(result.current.results).toEqual("mock-data");
   });
 });
